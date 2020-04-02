d05e56bd29f4e8cc596dea6bfb5d164c5fa9f896
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-02 17:24:01 +0100

    add helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def redirect_unless_logged_in
+        redirect_to root_path unless logged_in?
+    end
+
+    def redirect_if_logged_in
+        redirect_to root_path if logged_in?
+    end
+
+    def set_and_check_user
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            redirect_to root_path if @user != current_user
+        end
+    end
 end
