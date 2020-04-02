a38a070a98e4f6a5acb8a809accc7621d4ee0116
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-02 18:37:30 +0100

    declare application controller methods as helper method to use them outside the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
+    protect_from_forgery with: :exception
+
+    helper_method :current_user, :logged_in?, :redirect_unless_logged_in, :redirect_if_logged_in, :set_and_check_user
+
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
