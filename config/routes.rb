36214e44a03956f35c239f5ee0cc889cef9a1ec2
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-02 15:43:41 +0100

    add all required routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   
   root "welcome#home"
 
+  get "/users/new", to: "users#new", as: :signup
+
+  post "/sessions", to: "sessions#create"
+
+  get "/logout", to: "sessions#destroy"
+
+  get "/users/:id", to: "users#show"
+
+  get "/runs", to: "runs#index"
+
 end
