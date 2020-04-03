1a2bc33e706bed3c7d66ce2542f9e91b13b12645
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-03 16:51:12 +0100

    change sign up form and build users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
 
     def new
+        redirect_if_logged_in
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        binding.pry
+        # if @user.save
+        #     session[:user_id] = @user.user_id
+        #     redirect_to user_path
+        # else
+        #     render :new
+        # end
     end
     
     def show
+        redirect_unless_logged_in
+        @user = User.find_by(id: params[:id])
+        redirect_to root_path if @user != current_user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :email, :password)
     end
     
 end
