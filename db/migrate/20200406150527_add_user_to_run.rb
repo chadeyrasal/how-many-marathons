c149f14fc4a8aaa086ac36d04127386285baaa5e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-06 16:08:05 +0100

    add foreign key to runs referencing the user they belong to

---
+++ db/migrate/20200406150527_add_user_to_run.rb
@@ -0,0 +1,5 @@
+class AddUserToRun < ActiveRecord::Migration[6.0]
+  def change
+    add_column :runs, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_174016) do
+ActiveRecord::Schema.define(version: 2020_04_06_150527) do
 
   create_table "runs", force: :cascade do |t|
     t.datetime "date"
     t.float "distance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
