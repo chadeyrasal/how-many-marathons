1c014be83d45a0c4c83c181e3f10ba9f30257274
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-06 19:53:41 +0100

    set up new run form and controller action

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,6 +1,28 @@
 class RunsController < ApplicationController
 
+    before_action :redirect_unless_logged_in, only: [:index, new]
+
     def index
+        @runs = current_user.runs
+    end
+
+    def new
+        @run = Run.new
+    end
+
+    def create
+        @run = current_user.runs.new(run_params)
+        if @run.save
+            redirect_to user_path(current_user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def run_params
+        params.require(:run).permit(:date, :distance)
     end
     
 end
