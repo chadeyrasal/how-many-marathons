0300eec21c59e176ee1ef29e854c06bb14179d3e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-06 17:05:52 +0100

    create total_distance and last_five methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
     validates :email, uniqueness: true
 
+    def total_distance
+        total = 0
+        self.runs.each{|run| total += run.distance}
+        total
+    end
+
+    def last_five
+        self.runs.order(created_at: :desc).limit(5)
+    end
+
 end
